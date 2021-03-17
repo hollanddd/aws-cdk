9142f3bf5dde744694e7644f28526ed23703e20a
Darren <me@darrenholland.com>
2021-03-17 15:56:08 -0500

    topic rule grant publish

--- packages/@aws-cdk/aws-iot/lib/util.ts
+++ packages/@aws-cdk/aws-iot/lib/util.ts
@@ -1,8 +1,14 @@
-export { generatePolicyName } from '@aws-cdk/aws-iam/lib/util';
-import { Fn, Stack } from '@aws-cdk/core';
-import { IConstruct } from 'constructs';
+import { Fn, IConstruct, Stack, Arn } from '@aws-cdk/core';
 import { CertificateAttributes } from './certificate';
 import { ThingAttributes } from './thing';
+
+export { generatePolicyName } from '@aws-cdk/aws-iam/lib/util';
+import { IRole, PolicyStatement, ServicePrincipal, Role } from '@aws-cdk/aws-iam';
+export { undefinedIfAllValuesAreEmpty } from '@aws-cdk/core/lib/util';
+
+// keep this import separate from other imports to reduce chance for merge conflicts with v2-main
+// eslint-disable-next-line no-duplicate-imports, import/order
+import { Construct } from '@aws-cdk/core';
 
 export function parsePolicyArn(construct: IConstruct, policyName: string): string {
   return parseArn(construct, 'policy', policyName);
@@ -50,7 +56,7 @@ export function parseThingName(construct: IConstruct, props: ThingAttributes): s
     return props.thingName;
   }
 
-  // extract certificate id from certificate arn
+  // extract thingName from certificate arn
   if (props.thingArn) {
     return Stack.of(construct).parseArn(props.thingArn).resourceName?.replace('thing/', '') || '';
   }
@@ -68,8 +74,37 @@ function parseArn(construct: IConstruct, resource: string, resourceName: string)
   });
 }
 
-export { undefinedIfAllValuesAreEmpty } from '@aws-cdk/core/lib/util';
-
 export function parseRuleName(topicRuleArn: string): string {
   return Fn.select(1, Fn.split('rule/', topicRuleArn));
+}
+/**
+ * Obtain the Role for the Topic Rule
+ *
+ * If a role already exists, it will be returned. This ensures that if multiple
+ * events have the same target, they will share a role.
+ */
+export function singletonTopicRuleRole(scope: IConstruct, policyStatements: PolicyStatement[]): IRole {
+  const stack = Stack.of(scope);
+  const id = 'AllowIot';
+  const existing = stack.node.tryFindChild(id) as IRole;
+  if (existing) { return existing; }
+
+  const role = new Role(scope as Construct, id, {
+    assumedBy: new ServicePrincipal('iot.amazonaws.com'),
+  });
+
+  policyStatements.forEach(role.addToPolicy.bind(role));
+
+  return role;
+}
+
+export function topicArn(scope: IConstruct, topic: string, region?: string, account?: string): string {
+  const stack = Stack.of(scope);
+  return Arn.format({
+    region: region || stack.region,
+    account: account || stack.account,
+    service: 'iot',
+    resource: 'topic',
+    resourceName: topic,
+  }, stack);
 }
